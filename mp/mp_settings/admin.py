be058c96b1fc374ccdd16a16e604302049df8c28
unknown <sfletcher@ecotrust.org>
2013-06-26 20:34:39 -0700

    enabling url structure for project-name/planner and project-name/catalog.  ensuring initial json is project specific.  ensuring catalog View links are project specific (unless otherwise). adding migration script that adds slug_name to marineplannersettings.

--- mp/mp_settings/admin.py
+++ mp/mp_settings/admin.py
@@ -5,5 +5,6 @@ class MarinePlannerSettingsAdmin(admin.ModelAdmin):
     list_display = ('project_name', 'latitude', 'longitude', 'zoom', 'active')
     search_fields = ['project_name']
     ordering = ('project_name',)
+    exclude = ['slug_name']
 
 admin.site.register(MarinePlannerSettings, MarinePlannerSettingsAdmin)
