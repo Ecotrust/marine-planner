a7eac5e6da0297c83028c8616b9ecc6c105ecb9c
unknown <sfletcher@ecotrust.org>
2013-07-10 16:01:50 -0700

    changing marker path in map.js

--- media/js/map.js
+++ media/js/map.js
@@ -247,7 +247,7 @@ app.init = function () {
     app.markers = new OpenLayers.Layer.Markers( "Markers" );
     var size = new OpenLayers.Size(18,28);
     var offset = new OpenLayers.Pixel(-(size.w/2), -size.h);
-    app.markers.icon = new OpenLayers.Icon('/media/mp/img/red-pin.png', size, offset);
+    app.markers.icon = new OpenLayers.Icon('/media/img/red-pin.png', size, offset);
     app.map.addLayer(app.markers);
       
     //no longer needed?
