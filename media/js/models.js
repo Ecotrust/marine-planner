8282e91b1f04532148afc7a9d34d7e5acd1416ed
Scott Fletcher <sfletche@gmail.com>
2014-07-24 17:19:12 -0700

    fixing IE8 issue with 'default' attribute

--- media/js/models.js
+++ media/js/models.js
@@ -142,14 +142,14 @@ function layerModel(options, parent) {
     // opacity
     self.opacity.subscribe(function(newOpacity) {
         if (self.layer.CLASS_NAME === "OpenLayers.Layer.Vector") {
-            self.layer.styleMap.styles.default.defaultStyle.strokeOpacity = newOpacity;
-            self.layer.styleMap.styles.default.defaultStyle.graphicOpacity = newOpacity;
+            self.layer.styleMap.styles['default'].defaultStyle.strokeOpacity = newOpacity;
+            self.layer.styleMap.styles['default'].defaultStyle.graphicOpacity = newOpacity;
             //fill is currently turned off for many of the vector layers
             //the following should not override the zeroed out fill opacity
             //however we do still need to account for shipping lanes (in which styling is handled via lookup)
             if (self.fillOpacity > 0) {
                 var newFillOpacity = self.fillOpacity - (self.defaultOpacity - newOpacity);
-                self.layer.styleMap.styles.default.defaultStyle.fillOpacity = newFillOpacity;
+                self.layer.styleMap.styles['default'].defaultStyle.fillOpacity = newFillOpacity;
             }
             self.layer.redraw();
         } else {
